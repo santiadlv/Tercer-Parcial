eb7276d3df96d0b985ac1d34d38bdaab2c114dfd
santiadlv <49079265+santiadlv@users.noreply.github.com>
2019-05-02 12:46:12 -0600

    Implementation complete, final release

--- Parcial3_P1/Parcial3_P1/Program.cs
+++ Parcial3_P1/Parcial3_P1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Parcial3_P1.Clases;
 
 namespace Parcial3_P1
 {
@@ -10,6 +11,13 @@ namespace Parcial3_P1
     {
         static void Main(string[] args)
         {
+            Dog Marley = new Dog("Marley");
+            Marley.talk();
+            Console.WriteLine("Marley: {0}", Marley.talk());
+            Cat Fluffles = new Cat("Fluffles");
+            Fluffles.talk();
+            Console.WriteLine("Fluffles: {0}", Fluffles.talk());
+            Console.ReadKey();
         }
     }
 }

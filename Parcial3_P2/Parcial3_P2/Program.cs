9b0fe1a2b3c2e5478b28c0416ba33f4907b6e881
santiadlv <49079265+santiadlv@users.noreply.github.com>
2019-05-02 13:17:22 -0600

    Implementation complete, final release

--- Parcial3_P2/Parcial3_P2/Program.cs
+++ Parcial3_P2/Parcial3_P2/Program.cs
@@ -11,7 +11,38 @@ namespace Parcial3_P2
     {
         static void Main(string[] args)
         {
-            
+            int[] nums = { 5, 7, 9, 4, 6, 8 };
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i].ToString());
+            }
+
+            NormalClass.insertionSort(nums, 5);
+            Console.WriteLine();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i].ToString());
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+
+            string[] words = { "doggo", "cat", "turtle", "elephant", "bird" };
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+
+            GenericClass.insertionSort(words, 5);
+            Console.WriteLine();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+
+            Console.ReadKey();
         }
     }
 }
